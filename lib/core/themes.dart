72786551faf3e5ab5b283a4752680ddee9728970
zehranurkarogul <148694182+zehranurkarogul@users.noreply.github.com>
2025-01-25 21:38:13 +0300

    Tema yönetimi için themes.dart dosyası düzenlendi

---
+++ lib/core/themes.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+const colors = {
+  "primary": Color.fromARGB(255, 229, 192, 255), 
+  "onPrimary": Color.fromARGB(255, 255, 255, 255),
+  "secondary": Color.fromARGB(255, 229, 192, 255),  
+  "onSecondary": Color.fromARGB(255, 255, 255, 255),
+  "error": Color.fromARGB(255, 255, 0, 0),
+  "onError": Color.fromARGB(255, 241, 241, 241),
+  "surface": Color.fromARGB(255, 255, 255, 255),
+  "onSurface": Color.fromARGB(255, 255, 255, 255),
+};
+
+const darkColors = {
+  "primary": Color.fromARGB(255, 229, 192, 255), 
+  "onPrimary": Color.fromARGB(255, 0, 0, 0),  
+  "secondary": Color.fromARGB(255, 23, 33, 43), 
+  "onSecondary": Color.fromARGB(255, 241, 241, 241),  
+  "error": Color.fromARGB(255, 255, 0, 0),
+  "onError": Color.fromARGB(255, 241, 241, 241),
+  "surface": Color.fromARGB(255, 4, 4, 68),  
+  "onSurface": Color.fromARGB(255, 0, 0, 0),  
+};
+
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme(
+    brightness: Brightness.light, 
+    primary: colors["primary"]!, 
+    onPrimary: colors["onPrimary"]!, 
+    secondary: colors["secondary"]!, 
+    onSecondary: colors["onSecondary"]!, 
+    error: colors["error"]!, 
+    onError: colors["onError"]!, 
+    surface: colors["surface"]!, 
+    onSurface: colors["onSurface"]!
+    )
+);
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme(
+    brightness: Brightness.dark, 
+    primary: darkColors["primary"]!, 
+    onPrimary: darkColors["onPrimary"]!, 
+    secondary: darkColors["secondary"]!, 
+    onSecondary: darkColors["onSecondary"]!, 
+    error: darkColors["error"]!, 
+    onError: darkColors["onError"]!, 
+    surface: darkColors["surface"]!, 
+    onSurface: darkColors["onSurface"]!
+    )
+);
